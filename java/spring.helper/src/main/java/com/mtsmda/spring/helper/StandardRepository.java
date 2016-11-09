810c25532f05e6ad23569b3f55b3370ead5a4944
akbars95 <mynzat.dmitrii@gmail.com>
2016-11-10 00:00:18 +0200

    v - 1.1.2.RELEASE.09112016 - add standard operation repository

---
+++ java/spring.helper/src/main/java/com/mtsmda/spring/helper/StandardRepository.java
@@ -0,0 +1,18 @@
+package com.mtsmda.spring.helper;
+
+import com.mtsmda.spring.helper.response.CommonResponse;
+
+import java.util.List;
+
+/**
+ * Created by MTSMDA on 09.11.2016.
+ */
+public interface StandardRepository<T> {
+
+    CommonResponse<Boolean> insert(T tObject);
+    CommonResponse<Boolean> update(T tObject);
+    CommonResponse<Boolean> delete(T tObject);
+    CommonResponse<T> getById(Integer id);
+    CommonResponse<List<T>> getAll();
+
+}
